36bca1aaa5a5e4caf1535de040fb5c2ff30741e6
Dinesh Deore <113936381+Dinesh-Deore1@users.noreply.github.com>
2023-09-17 10:45:33 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,12 @@
+a=67
+b=891
+c=90
+if a>b :
+    if(a>c):
+        print("a is greater")
+    else:
+        print("c is greater")
+elif b>c:
+    print("b is greater")
+else:
+    print("c is greater")
